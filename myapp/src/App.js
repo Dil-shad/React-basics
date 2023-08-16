67e95afc657d4d1b8a1d48acd6fa90a3403d13a9
Dilshad Abdu Samad <dlz611606@gmail.com>
2023-08-16 14:01:10 +0530

    react-components-importing and reuseblity

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,51 +1,16 @@
 import "./App.css";
+import Header from "./components/Header.js";
+import Home from "./components/Home.js";
+
 
 function App() {
-  const name = "jay";
-  var a = 15;
-  var b = 20;
-  console.log(a);
-  const massage = () => {
-    return "good morning";
-  };
   return (
-    <>
-      <table>
-        <Row />
-        <Column />
-      </table>
-    </>
+    <div className="App">
+      <Header></Header>
+      <Home />
+      
+    </div>
   );
 }
 
-function Row(params) {
-  return (
-    <>
-      <tr>
-        <th>Name</th>
-        <th>Address</th>
-      </tr>
-    </>
-  );
-}
-function Column(params) {
-  return (
-    <>
-      <tr>
-        <td>Dilshad</td>
-        <td>Puthen</td>
-      </tr>
-    </>
-  );
-}
 export default App;
-
-{
-  /* <div className="App">
-<h1 className="heading">Hello world</h1>
-<h1> MY name is {name} </h1>
-<h3>{10 + 20}</h3>
-<h2>{a + b}</h2>
-<h5>{massage}</h5>
-</div> */
-}
