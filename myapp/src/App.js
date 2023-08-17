3c3042958b7a4ef813e3eaf7662c10be02106c1d
Dilshad Abdu Samad <dlz611606@gmail.com>
2023-08-17 12:09:58 +0530

    we have install bootsrtap fist`npm install react-bootstrap bootstrap` then add this to app.js `import 'bootstrap/dist/css/bootstrap.min.css';`

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.js";
 import Home from "./components/Home.js";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home />
+      
       
     </div>
   );
--- myapp/src/components/Home.js
+++ myapp/src/components/Home.js
@@ -9,13 +9,7 @@ const Home = () => {
   return (
     <div>
       <h1 className={homeCss.header}>Home</h1>
-      <h1 style={{ color: "red" }}>Home Component</h1>
-      <h2 style={headingStyle}>this is for variable css</h2>
-      <Child />
-      <button className={btnCss.loginBtn} type="button">Log in</button>
-      <button className={btnCss.logoutBtn} type="button">Log out</button>
-  
-
+      
     </div>
   )
 }
