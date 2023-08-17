08438b0e746d6362e1902ecff5d2c63307bc100a
Dilshad Abdu Samad <dlz611606@gmail.com>
2023-08-17 17:43:46 +0530

    event done with examples

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,103 +1,45 @@
-import React from "react";
-import headerCss from "./Header.module.css";
-//import Button from 'react-bootstrap/Button'; for single component
-import {
-  Button,
-  Table,
-  Accordion,
-  Card,
-  Navbar,
-  Container,
-  Nav,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Container } from "react-bootstrap";
+
 const Header = () => {
+  const [selectedOption, setSelectedOption] = useState("option2");
+
+  const clickHandle = () => {
+    console.log("you clicked");
+  };
+
+  const dbClickHandle = (arg) => {
+    alert("Hello " + arg);
+  };
+
+  const onChangeHandle = (e) => {
+    alert("You changed value: " + e.target.value);
+  };
+  const mouseHoverHandle = (e) => {
+    alert("Mouse Over " )
+  };
+  const onKeyDown = (e) => {
+    alert("You pressed " + e.key);
+  }
   return (
     <>
-      <Container>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-     
-      <h1>Hello World</h1>
-      <Button variant="primary">Primary</Button>
-      <br />
-      <br />
+      <Container fluid >
+        <Button className="mt-5" variant="primary" onClick={clickHandle}>
+          Click Me
+        </Button>
+        <h1 onDoubleClick={() => dbClickHandle("Dilshad")}>Double clicked</h1>
+        <select className=" " style={{width:'180px'}} onChange={onChangeHandle} >
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2 (Selected)</option>
+          <option value="option3">Option 3</option>
+        </select><br />
+        <input className="mt-2" type="text"  onChange={onChangeHandle} placeholder="OnchangeHandler"/><br />
+         {/* <h1 onMouseOver={mouseHoverHandle}>Mouse Hover Test</h1> */}
+         <input type="text" onKeyDown={onKeyDown} placeholder="Test keys"/>
 
-      <Table striped bordered hover size="sm" className="">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
 
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="https://picsum.photos/200/300" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+
       </Container>
     </>
   );
