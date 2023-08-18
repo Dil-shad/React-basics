9224ec62b311e0f51a740129c4f91b4b59c58c07
Dilshad Abdu Samad <dlz611606@gmail.com>
2023-08-18 16:48:37 +0530

    prop method using js map function

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,13 +6,17 @@ import States from "./components/States.js";
 import { Container } from "react-bootstrap";
 
 function App() {
+  const states = [
+    { name: "Kerala", language: "Malayalam", population: 1000 },
+    { name: "Tamilnadu", language: "Tamil", population: 2000 },
+    { name: "Andra", language: "Telugu", population: 3000 },
+    { name: "Delhi", language: "Hindi", population: 5000 },
+  ];
+
   return (
     <div className="App">
       <Container>
-        <States name={"Kerala"} lang={"Malayalam"} population={1000}/>
-        <States name={"Tamilnadu"} lang={"Tamil"} population={2000} />
-        <States name={"Karnataka"} lang={"Kannada"} population={3000} />
-        <States name={"Goa"} lang={"Goa"} population={5000} />
+        <States States={states} />
       </Container>
     </div>
   );
--- myapp/src/components/States.js
+++ myapp/src/components/States.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-const States = ({name,lang,population}) => { 
+const States = ({States}) => { 
   //insteed of props object we can declare varibles {name,lang,popuylation}
   return (
-    <div className='bg-warning'>
-      <h1>{name}</h1>
-      <h4>Language:{lang}</h4>
-      <h4>Population:{population}</h4>
+    <div>
+      {States.map((s)=>(
+        <div className='bg-warning'>
+        <h1>{s.name}</h1>
+        <h4>Language:{s.language}</h4>
+        <h4>Population:{s.population}</h4>
+      </div>
+
+      ))
+    }
     </div>
+   
   )
 }
 
